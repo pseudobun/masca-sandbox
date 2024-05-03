1116563dbdca68c5768cc870e6e9e1c8cd68e91e
Urban Vidovič <urbanfoundit@gmail.com>
2024-05-03 18:06:50 +0200

    feat: include shadcn
    Signed-off-by: Urban Vidovič <urbanfoundit@gmail.com>

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useState, type ReactNode } from 'react';
 import { WagmiProvider } from 'wagmi';
 import { NextUIProvider } from '@nextui-org/react';
+import { Toaster } from '@/components/ui/sonner';
 
 import { config } from '@/wagmi';
 
@@ -11,12 +12,15 @@ export function Providers(props: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <WagmiProvider config={config}>
-      <NextUIProvider>
-        <QueryClientProvider client={queryClient}>
-          {props.children}
-        </QueryClientProvider>
-      </NextUIProvider>
-    </WagmiProvider>
+    <div>
+      <WagmiProvider config={config}>
+        <NextUIProvider>
+          <QueryClientProvider client={queryClient}>
+            {props.children}
+          </QueryClientProvider>
+        </NextUIProvider>
+      </WagmiProvider>
+      <Toaster />
+    </div>
   );
 }
